288637de891abf75eb2e602ce66af2776cb1742a
Динара Лаврова <lavrovadinara18@mail.ru>
2023-03-21 20:46:08 +0300

    add working version without classes

---
+++ defence.py
@@ -0,0 +1,10 @@
+from random import randint
+
+
+def defence(char_name, char_class):
+    if char_class == 'warrior':
+        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
+    if char_class == 'mage':
+        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
+    if char_class == 'healer':
+        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
